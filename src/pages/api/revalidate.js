3779e1bbd6b68a09117aa47c6485fb8e24bcd8d2
SebEdena <viguier.sebastien@orange.fr>
2022-04-23 18:56:48 +0200

    try fix revalidate

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -3,7 +3,7 @@ import { getAllPostsWithSlug } from '../../lib/api'
 export default async function handler(req, res) {
     // Check for secret to confirm this is a valid request
     if (req.query.secret !== process.env.NEXT_PUBLIC_REVALIDATE_TOKEN) {
-        return res.status(401).json({ message: 'Invalid token' })
+        return res.status(401).json({ message: 'Invalid token', data: [req.query.secret, process.env.NEXT_PUBLIC_REVALIDATE_TOKEN] })
     }
   
     try {
@@ -14,7 +14,6 @@ export default async function handler(req, res) {
         await res.unstable_revalidate('/posts/[slug]')
         return res.json({ revalidated: true })
     } catch (err) {
-        console.log(err);
         // If there was an error, Next.js will continue
         // to show the last successfully generated page
         return res.status(500).send('Error revalidating')
