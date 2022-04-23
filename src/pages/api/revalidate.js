0b7cf87d7cc7823293ea945ee639f0e9055b111e
SebEdena <viguier.sebastien@orange.fr>
2022-04-23 19:03:52 +0200

    remove unnecessary code

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -3,7 +3,7 @@ import { getAllPostsWithSlug } from '../../lib/api'
 export default async function handler(req, res) {
     // Check for secret to confirm this is a valid request
     if (req.query.secret !== process.env.NEXT_PUBLIC_REVALIDATE_TOKEN) {
-        return res.status(401).json({ message: 'Invalid token', data: [req.query.secret, process.env.NEXT_PUBLIC_REVALIDATE_TOKEN] })
+        return res.status(401).json({ message: 'Invalid token' })
     }
   
     try {
