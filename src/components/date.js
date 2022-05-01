a8fda6269a7af605ccfae8f695c4a9b1f6588c91
SebEdena <viguier.sebastien@orange.fr>
2022-05-01 20:13:01 +0200

    tweaks

--- src/components/date.js
+++ src/components/date.js
@@ -1,9 +1,10 @@
 import { format } from 'date-fns'
+import { fr } from 'date-fns/locale'
 
 export default function DateComponent({ dateString }) {
   return (
     <time dateTime={dateString}>
-      {format(new Date(dateString), 'LLLL	d, yyyy', { locale: 'fr-FR'})}
+      {format(new Date(dateString), 'LLLL	d, yyyy', { locale: fr })}
     </time>
   )
 }
