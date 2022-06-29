f0c1c304d7eda80777b48e607d9774cd80da4843
SebEdena <viguier.sebastien@orange.fr>
2022-06-29 19:50:17 +0200

    Refactor PWA & other icons

--- src/components/meta/site-meta.js
+++ src/components/meta/site-meta.js
@@ -3,33 +3,21 @@ import Head from 'next/head'
 export default function SiteMeta() {
   return (
     <Head>
-      {/* <link
+      <link
         rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+        sizes="192x192"
+        href="/favicon/mobile_192.png"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon/favicon-16x16.png"
-      /> */}
       <link rel="manifest" href="/favicon/site.webmanifest" />
       <link
         rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
+        href="/favicon/mobile_512.png"
         color="#000000"
       />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
+      <link rel="shortcut icon" href="/favicon/favicon.svg" />
+      <meta name="msapplication-TileColor" content="#3b82f6" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
+      <meta name="theme-color" content="#3b82f6" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <link rel='icon' href='/favicon/favicon.svg'></link>
     </Head>
