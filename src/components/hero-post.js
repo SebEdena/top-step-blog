8c6966d673c9cace05088ba325765de83336312e
SebEdena <viguier.sebastien@orange.fr>
2022-07-10 12:27:22 +0200

    fix hero post side menu

--- src/components/hero-post.js
+++ src/components/hero-post.js
@@ -16,14 +16,14 @@ export default function HeroPost({
       <div className='relative'>
         <CoverImage imgClasses="rounded-md" title={title} slug={slug} url={coverImage.url} />
         <div className="bottom-0 absolute w-full">
-          <div className="gradient h-16 from-black/80 to-black/0 bg-gradient-to-t"></div>
-          <div className='p-4 md:p-8 pt-0 md:pt-0 rounded-b-md text-white bg-black/80'>
-            <h3 className="text-xl lg:text-3xl leading-tight">
+          <div className="gradient h-6 md:h-16 from-black/80 to-black/0 bg-gradient-to-t"></div>
+          <div className='p-3 md:p-8 pt-0 md:pt-0 rounded-b-md text-white bg-black/80'>
+            <h3 className="text-sm md:text-xl lg:text-3xl leading-tight">
               <Link href={`/posts/${slug}`}>
                 <a className="hover:underline decoration-2">{title}</a>
               </Link>
             </h3>
-            <div className="mt-4 text-md lg:text-lg flex flex-row justify-between">
+            <div className="mt-4 text-xs md:text-md lg:text-lg flex flex-row justify-between">
               <Tag tag={tags[0]} />
               <DateComponent dateString={date} />
             </div>
