907d8561489ac3b45b08fd5e6e2d0a6aa72a467f
SebEdena <viguier.sebastien@orange.fr>
2022-10-10 21:20:40 +0200

    json parse body

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     // Check for secret to confirm this is a valid request
     if (req.method === 'POST') {
         
-        if (req.body.secret !== process.env.NEXT_PUBLIC_REVALIDATE_TOKEN) {
+        if (JSON.parse(req.body).secret !== process.env.NEXT_PUBLIC_REVALIDATE_TOKEN) {
             return res.status(401).json({ message: 'Invalid token' })
         }
     
